0e56385de23e7dd0aa40f0d1f0dd2f0263949f25
mistvanp <marai.istvanp@gmail.com>
2022-08-16 10:11:31 +0200

    half done

--- js/01/objectsMerge.js
+++ js/01/objectsMerge.js
@@ -1,3 +1,29 @@
-const objectsMerge = () => {};
+const johnDoe = { 
+    firstName: 'John',
+    lastName: 'Doe'
+};
+
+const janeDoe = { 
+    firstName: 'Jane',
+    lastName: 'Doe'
+}
+
+
+const objectsMerge = (johnDoe,janeDoe) => {
+
+
+}
+
 
 export default objectsMerge;
+
+
+
+
+// ## 1. feladat
+// - A `js/01/objectsMerge.js` fájlban dolgozz.
+// - Írj egy függvényt, amely paraméterként tetszőleges darabszámú objektumot 
+// merge-öl össze, majd visszatér ezzel az objektummal!
+// - A függvény neve `objectsMerge` legyen!
+// Az összefűzést úgy hajtsa végre, hogy az új objektum property-jei egyszerű indexek legyenek (0, 1, 2 stb.), amelyek tartalmazzák 
+// a részobjektumokat!  
